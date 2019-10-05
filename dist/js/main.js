a99e1fef3f6694fc63ab3dd8543042887e856980
Aaron P. Asuncion <aaronpasuncion@gmail.com>
2019-10-04 20:11:10 -0400

    responsiveness for large devices and nav overlay hide when link is clicked

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,9 @@
 var openOverlay = document.getElementById("open-overlay");
 var navOverlay = document.querySelector("#overlay");
 var body = document.querySelector("body");
+var overlayLinks = document.querySelectorAll(
+  "#overlay-nav > .nav-item > .nav-link"
+);
 
 openOverlay.addEventListener("click", () => {
   navOverlay.classList.contains("hidden")
@@ -11,3 +14,11 @@ openOverlay.addEventListener("click", () => {
       navOverlay.classList.remove("visible"),
       body.classList.remove("no-scroll"));
 });
+
+for (var i = 0; i < overlayLinks.length; i++) {
+  overlayLinks[i].addEventListener("click", () => {
+    navOverlay.classList.add("hidden"),
+      navOverlay.classList.remove("visible"),
+      body.classList.remove("no-scroll");
+  });
+}
